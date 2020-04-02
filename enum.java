3ec795b8bb899f9a2af89bd4d171b8f012ebb9c2
yanamalaumesh1874 <62681339+yanamalaumesh1874@users.noreply.github.com>
2020-04-02 20:07:19 +0530

    Create enum.java

---
+++ enum.java
@@ -0,0 +1,12 @@
+class enumex
+{                          
+enum Mobile
+{
+	SAMSUNG,VIVO,APPLE;
+}
+public static void main(String[] args)
+{
+	Mobile I=Mobile.VIVO;
+     System.out.println(I);
+}
+}
